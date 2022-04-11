acdd3688172e8fc60f4a2025e7d2af1ac8c37b29
fahad <fahad.ibrahim@clustox.com>
2022-04-11 14:32:47 +0500

    php-mysql-debug-mongodb

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,43 @@ RUN apt-get update && apt-get upgrade -y
 RUN apt-get install -y autoconf pkg-config libssl-dev
 RUN pecl install mongodb-1.7.4
 RUN pecl install xdebug-2.8.0
+RUN docker-php-ext-enable xdebug
+COPY xdebug.ini /usr/local/etc/php/conf.d/xdebug.ini
+# COPY error_reporting.ini /usr/local/etc/php/conf.d/error_reporting.ini
+
+# RUN mkdir -p docker/php/conf.d
+# RUN touch docker/php/conf.d/xdebug.ini
+# RUN touch docker/php/conf.d/error_reporting.ini
+# zend_extension=xdebug
+
+
+# COPY . /home/xdebug/xdebug-debug.ini
+# COPY . /home/xdebug/xdebug-default.ini
+# COPY . /home/xdebug/xdebug-off.ini
+# COPY . /home/xdebug/xdebug-profile.ini
+# COPY . /home/xdebug/xdebug-trace.ini
+
+# ARG XDEBUG_MODES
+# ARG REMOTE_HOST="host.docker.internal"
+# ARG REMOTE_PORT=9003
+# ARG IDE_KEY="docker"
+
+# ENV MODES=$XDEBUG_MODES
+# ENV CLIENT_HOST=$REMOTE_HOST
+# ENV CLIENT_PORT=$REMOTE_PORT
+# ENV IDEKEY=$IDE_KEY
+
+# COPY ./docker/php/fpm-entrypoint.sh /home/fpm-entrypoint
+# RUN chmod +x /home/fpm-entrypoint
+
+# WORKDIR /var/www/html
+
+# ENTRYPOINT ["/home/fpm-entrypoint"]
+
+# [xdebug]
+#xdebug.mode=develop,debug
+#xdebug.client_host=host.docker.internal
+#xdebug.start_with_request=yes
 #RUN apt-get install pdo-mysql
 RUN echo "extension=mongodb.so" >> /usr/local/etc/php/conf.d/mongodb.ini
 
--- index.php
+++ index.php
@@ -14,53 +14,51 @@ $pass = 'MYSQL_PASSWORD';
 // database name
 $mydatabase = 'MYSQL_DATABASE';
 // check the mysql connection status
-
-try {
-  $conn = new PDO("mysql:host=localhost;dbname=$mydatabase", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully\n";
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage() . "\n";
-}
-/*
 $conn = new mysqli($host, $user, $pass, $mydatabase);
 
-// select query
-$sql = 'SELECT * FROM users';
-
-if ($result = $conn->query($sql)) {
-    while ($data = $result->fetch_object()) {
-        $users[] = $data;
-    }
+if ($conn->connect_errno) {
+    die("Connect Error: " . $conn->connect_error);
 }
+else{
+  echo "<br><br>";
+  echo "\nConnected with MYSQL"; 
+  echo "<br>";
+	$sql = "create table users (id int(6), username VARCHAR(30) ,password VARCHAR(30))";
+	if ($conn->query($sql) === TRUE) {
+		echo "Table users created successfully";
+		echo "<br>";
+	}
 
-foreach ($users as $user) {
-    echo "<br>";
-    echo $user->username . " " . $user->password;
-    echo "<br>";
+	$sql = "insert into users (username, password) values ('Alice','this is my password'), ('Job','12345678')";
+	
+	if ($conn->query($sql) === TRUE) {
+		echo "Data inserted successfully";
+		echo "<br>";
+	}
+	$sql = 'SELECT * FROM users';
+	
+	if ($result = $conn->query($sql)) {
+		while ($data = $result->fetch_object()) {
+			$users[] = $data;
+		}
+	}
+	
+	foreach ($users as $user) {
+		echo "<br>";
+		echo $user->username . " " . $user->password;
+		echo "<br>";
+	}
 }
-*/
+echo "<br>";
+
 echo extension_loaded("mongodb") ? "loaded\n" : "not loaded\n";
 
   //  // connect to mongodb
     $manager = new MongoDB\Driver\Manager("mongodb://root:password@mongo:27017");
 
-   echo "Connection to database successfully";
+   echo "Connection to Mongo database successfully";
 
 
-// // Query Class
-// $query = new MongoDB\Driver\Query(array('likes' => 100));
-
-// // Output of the executeQuery will be object of MongoDB\Driver\Cursor class
-// $cursor = $manager->executeQuery('examplesdb.examplesCol', $query);
-
-// // Convert cursor to Array and print result
-// print_r($cursor->toArray());
-
-// foreach($cursor as $r){
-//   print_($r);
-// }
 
 $filter = ['id' => 1];
 $options = [
@@ -71,35 +69,5 @@ $rows = $manager->executeQuery('examplesdb.users', $query); // $mongo contains t
 
 print_r($rows->toArray());
 
-   // select a database
-  //  $db = $m->examplesdb;
- 
-  //  echo "Database examplesdb selected";
- 
-
-//  MongoDB connection
-
- // PHP version 7.4 used here
-//  try {
-//   // connect to OVHcloud Public Cloud Databases for MongoDB (cluster in version 4.4, MongoDB PHP Extension in 1.8.1)
-//   $m = new MongoDB\Driver\Manager('mongodb://root:password@mongo:27017/admin?tls=true');
-// //  if($m->connection_status) 
-//     //echo "Mongo Conected to database successfully";
-//     $db = $m->examplesdb;
-//     $collection = $db->examplescol;
-
-//     $cursor = $collection->find();
-//   // iterate cursor to display title of documents
-//   foreach ($cursor as $document) {
-//   echo $document["title"] . "\n";
-//  }
-// }
-// catch (Throwable $e) {
-//   // catch throwables when the connection is not a success
-//   echo "Captured Throwable for connection : " . $e->getMessage() . PHP_EOL;
-// }
-
-
 
 ?>
-
